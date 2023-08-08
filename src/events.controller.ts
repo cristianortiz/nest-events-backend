a37eaa1d99292bcc7954d0a53b2de6a5614856df
cristianortiz <cristian.ortiznavia82@gmail.com>
2023-08-08 11:09:26 -0400

    - simple in memory data to test API Rest Methods

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -10,34 +10,62 @@ import {
 } from '@nestjs/common';
 import { CreateEventDto } from './create-event.dto';
 import { UpdateEventDto } from './update-event.dto';
+import { Event } from 'event.entity';
 
 @Controller('/events')
 export class EventsController {
+  //array of events
+  private events: Event[] = [];
+
+  //just for ilustration purpose in memory data
+
   @Get()
   findAll() {
-    return [
-      { id: 1, name: 'First event' },
-      { id: 2, name: 'Second event' },
-      { id: 3, name: 'Third event' },
-    ];
+    return this.events;
   }
 
   @Get(':id')
   findOne(@Param(':id') id) {
-    return { id: 1, name: 'First event' };
+    const event = this.events.find((event) => event.id === parseInt(id));
+    return event;
   }
 
   @Post()
   create(@Body() input: CreateEventDto) {
-    return input;
+    //JS object, spread syntax to assing it all the properties in request input, change the 'when' datatype to date
+    // because of the definition of Event class
+    const event = {
+      ...input,
+      when: new Date(input.when),
+      id: this.events.length + 1,
+    };
+    //add the new resource to events array and then return it as response
+    this.events.push(event);
+    return event;
   }
 
   @Patch(':id')
   update(@Param(':id') id, @Body() input: UpdateEventDto) {
-    return input;
+    //first find out if requested id resource is existing
+    const index = this.events.findIndex((event) => event.id === parseInt(id));
+    //spread syntax, update the resource in index position in events array
+    //copy the actual event properties and then update them with the input properties
+    //also updates the 'when' property if exists in input object creates a new Date obj, if is not
+    //the original 'when' value remains ('when'property is optional)
+    this.events[index] = {
+      ...this.events[index],
+      ...input,
+      when: input.when ? new Date(input.when) : this.events[index].when,
+    };
+
+    return this.events[index];
   }
 
   @Delete(':id')
   @HttpCode(204)
-  remove(@Param(':id') id) {}
+  remove(@Param(':id') id) {
+    //using filter function to remove the resource form events array, filter creates a new events array
+    // but without the filtered resource
+    this.events = this.events.filter((event) => event.id !== parseInt(id));
+  }
 }
