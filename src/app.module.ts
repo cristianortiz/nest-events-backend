35277850922c6499098b378455d39ce4d195fe1c
cristianortiz <cristian.ortiznavia82@gmail.com>
2023-08-09 10:06:16 -0400

    set up docker for DB connection and typeORM intro

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsController } from './events.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 //Modules encapsulate app building blocks  likes services controllers, test,entities etc
 //Module is a class annotated with model decorator
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: '127.0.0.1',
+      port: 3307,
+      username: 'root',
+      password: 'root',
+      database: 'nest-events',
+    }),
+  ],
   controllers: [AppController, EventsController],
   providers: [AppService],
 })
